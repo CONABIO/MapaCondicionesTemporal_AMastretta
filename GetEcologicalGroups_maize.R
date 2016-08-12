3a3061edf8205c011579a4aa5a5c4947adeaf88c
Alicia Mastretta-Yanes <a.mstt.yanes@gmail.com>
2016-08-12 13:03:46 -0500

    create script

---
+++ GetEcologicalGroups_maize.R
@@ -0,0 +1,13 @@
+### Obtain conditions for maize ecological groups according to Ruiz et al 2008
+### and extract this information for data Global maize project
+### Script by Alicia Mastretta Yanes
+### Ref: Ruiz Corral, José Ariel, Noé Durán Puga, José de Jesús Sánchez González, José Ron Parra, Diego Raymundo González Eguiarte, J.B. Holland, y Guillermo Medina García. “Climatic Adaptation and Ecological Descriptors of 42 Mexican Maize Races”. Crop Science 48, núm. 4 (2008): 1502. doi:10.2135/cropsci2007.09.0518.
+
+# Load data
+
+
+
+plot(Tmean, col=rev(brewer.pal(11, "RdYlBu")), main="Mean Temperature May-Oct (°C)")
+plot(Pmean, col=brewer.pal(11, "YlGn"), main="Mean rainfall May-Oct (mm)")
+
+

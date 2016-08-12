35f2614006f64a7cd5be029ce6f14d712e48e512
Alicia Mastretta-Yanes <a.mstt.yanes@gmail.com>
2016-08-12 14:17:40 -0500

    add projection

--- GetEcologicalGroups_maize.R
+++ GetEcologicalGroups_maize.R
@@ -3,11 +3,30 @@
 ### Script by Alicia Mastretta Yanes
 ### Ref: Ruiz Corral, José Ariel, Noé Durán Puga, José de Jesús Sánchez González, José Ron Parra, Diego Raymundo González Eguiarte, J.B. Holland, y Guillermo Medina García. “Climatic Adaptation and Ecological Descriptors of 42 Mexican Maize Races”. Crop Science 48, núm. 4 (2008): 1502. doi:10.2135/cropsci2007.09.0518.
 
-# Load data
+## Needs
+library(raster)
+library(RColorBrewer)
 
+### Load data
 
+## ENV
+# Get mean temp and rainfall for May-Oct. 
+# This was extracted from Cuervo et al 2013 data 
+# using the script Get_PrecipitationTemp_TemporalSeason.R
 
-plot(Tmean, col=rev(brewer.pal(11, "RdYlBu")), main="Mean Temperature May-Oct (°C)")
-plot(Pmean, col=brewer.pal(11, "YlGn"), main="Mean rainfall May-Oct (mm)")
+TmpPrec<-stack("Precipmean_MayOct.asc", "Tmean_MayOct.asc")
 
+# Change projection
+proj4string(TmpPrec) <- CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs +towgs84=0,0,0")
+
+# Maize
+maizedb<-read.delim("BaseMaicesNativos.txt")
+
+## Plot
+
+plot(TmpPrec$Tmean_MayOct, col=rev(brewer.pal(11, "RdYlBu")), main="Mean Temperature May-Oct (°C)")
+points(maizedb$Longitud, maizedb$Latitud, cex=0.3, pch=20)
+
+plot(TmpPrec$Precipmean_MayOct, col=brewer.pal(9, "YlGn"), main="Mean rainfall May-Oct (mm)")
+points(maizedb$Longitud, maizedb$Latitud, cex=0.3, pch=20)
 
